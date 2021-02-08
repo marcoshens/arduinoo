8963ac3106a0a0c57567b11951ff5f948ec05c1a
marcoshens <78345826+marcoshens@users.noreply.github.com>
2021-02-08 12:35:34 +0100

    Update snippet_killswitch.cpp

--- snippet_killswitch.cpp
+++ snippet_killswitch.cpp
@@ -3,5 +3,34 @@
 int switchState = 0;
 bool isTheButtonBeingPressed = false;
 bool play = false;
-
+// revisar que el pin del botón es este
 int buttonPin=4;
+
+
+//incluir en el setup ()
+
+pinMode(buttonPin,INPUT);
+
+//incluir en el loop (sustituyendo)
+
+void loop() {
+  checkButton();
+  if (play) {
+   // poner aquí la función que toque ejecutar
+  }
+}
+
+// Función de checkbutton
+
+void checkButton(){
+  switchState = digitalRead(buttonPin);
+  if (switchState == HIGH){
+    if (isTheButtonBeingPressed == false){
+      play = !play;
+      isTheButtonBeingPressed = true;
+  }
+}
+else{
+  isTheButtonBeingPressed = false;
+}
+}
