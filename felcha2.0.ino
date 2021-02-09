b96fc3913e0158054f7da5521a7f7fd880d56354
marcoshens <78345826+marcoshens@users.noreply.github.com>
2021-02-09 13:08:09 +0100

    Add files via upload

---
+++ felcha2.0.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo myServo;
+int const potPin = A0; 
+int potVal;
+int angle;
+
+void setup() {
+  myServo.attach(9);
+  Serial.begin(9600);
+}
+void loop() {
+ potVal = analogRead (potPin);
+ Serial.print("potVal: ");
+ Serial.print(potVal);
+angle = map(potVal, 100, 200, 0, 180);
+Serial.print(", angle: ");
+Serial.println(angle);
+myServo.write(angle);
+delay(500);
+}
