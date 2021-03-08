4fd3730a83a2845dd918e10ac5d9804b14ae1b08
marcoshens <78345826+marcoshens@users.noreply.github.com>
2021-03-08 10:50:15 +0100

    Add files via upload

--- media_de_notas.ino
+++ media_de_notas.ino
@@ -11,10 +11,9 @@
 */
 int Notas[]= {5,8,9,4,3};
 int numeroNotas = 5;
-//VICTOR RODRIGUEZ
 void setup(){
   int sumaNotas = 0;
-  for(int i = 0;i >numeroNotas;i++){
+  for(int i = 0;i <numeroNotas;i++){
     sumaNotas= sumaNotas+Notas[i];
   //sumaNotas = sumaNotas+Notas[0]
   //sumaNotas = sumaNotas+ sumaNotas[13]
@@ -24,7 +23,7 @@ float mediaNotas = sumaNotas/numeroNotas;
  Serial.begin(9600);
  Serial.print("Nota media):");
  Serial.print(mediaNotas);
- 
+
  }
 
  void loop(){
