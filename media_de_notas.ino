e4a456c7c0c69320c5dee55f0b8b303298514fc6
marcoshens <78345826+marcoshens@users.noreply.github.com>
2021-03-08 12:27:31 +0100

    Add files via upload

--- media_de_notas.ino
+++ media_de_notas.ino
@@ -9,23 +9,36 @@
 *  
 *  Hardware necesario:ninguno
 */
-int Notas[]= {5,8,9,4,3};
+int Notas[]= {9,10,5,8,6};
 int numeroNotas = 5;
 void setup(){
   int sumaNotas = 0;
+  int notaminima = 10;
   for(int i = 0;i <numeroNotas;i++){
     sumaNotas= sumaNotas+Notas[i];
   //sumaNotas = sumaNotas+Notas[0]
   //sumaNotas = sumaNotas+ sumaNotas[13]
   //asi repetibamente sumando +1 hasta que se quede sin elementos.
-  }
+
+  if (Notas[i] <notaminima) {
+   notaminima = Notas[i]; 
+  } //llave del if del mínimo
+  } //llave del for
 float mediaNotas = sumaNotas/numeroNotas;
  Serial.begin(9600);
  Serial.print("Nota media):");
- Serial.print(mediaNotas);
-
+ Serial.println(mediaNotas);
+ Serial.print("notaminima   ");
+ Serial.println(notaminima);
+ Serial.println("calculo de si toca hacer el examen con el operador y");
+ if (mediaNotas >= 5 && notaminima > 3) {
+  Serial.println("no toca hacer el examen de acceso"); 
  }
+ else{
+  Serial.println("toca hacer el examen de acceso");
+ }
+ }// llave del setup
 
+ 
  void loop(){
-  
  }
